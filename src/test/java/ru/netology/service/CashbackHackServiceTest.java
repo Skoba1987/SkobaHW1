628fc78c3cbee14d014d522e60e5aa1cadc79a84
Skoba Kristina <105549745+Skoba1987@users.noreply.github.com>
2022-09-25 18:35:37 +0300

    Update CashbackHackServiceTest.java

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,4 +1,29 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
 import static org.testng.Assert.*;
+
 public class CashbackHackServiceTest {
-    
+
+    @Test
+    public void remain() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 5700;
+        int actual = service.remain(amount);
+        int expected = 300;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void remainTwo() {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 3000;
+
+        int expected = 0;
+        int actual = service.remain(amount);
+
+        assertEquals(actual, expected);
+
+    }
 }
